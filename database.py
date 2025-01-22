7e5861c837575e29d84b4efa287075582bbbab3b
Edier55 <edierguerra55@gmail.com>
2025-01-22 12:38:15 -0500

    feat(database): agrega la configuración inicial de la base de datos

---
+++ database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+from decouple import config
+
+# url de base de datos
+URL_DB = f"mysql+pymysql://{config("MySQL_USER")}:{config("MySQL_PASSWORD")}@{config("MySQL_HOST")}:3306/{config("MySQL_DB")}"
+
+# Motor de base de datos 
+engine = create_engine(url=URL_DB,echo=True)
+
+# Fabrica de sesiones
+SessionLocal = sessionmaker(bind = engine)
+
+if __name__ == "__main__":
+    # Iniciar la base de datos
+    Base.metadata.create_all(engine)
+
+# Dependencia para fastapi
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
