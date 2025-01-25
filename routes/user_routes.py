12e1f38af2eb1cf814c549c421aae6ebafeb8c04
Edier55 <edierguerra55@gmail.com>
2025-01-25 10:48:22 -0500

    refactor(routes): añade encriptamiento a la contraseña del usuario

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,20 +1,23 @@
 from fastapi import Depends,APIRouter
-from schemas import UserModel,CreateUserResponse
 from typing import Annotated
-from database import get_db
 from sqlalchemy.orm import Session
+from utils.hashing import hash_password
+from schemas import UserModel,CreateUserResponse
 from models import User
+from database import get_db
 
 route = APIRouter(prefix="/user")
 
 
 @route.post("/create",response_model=UserModel)
 async def user(user_data:CreateUserResponse,db:Annotated[Session, Depends(get_db)]):
+    user_password_hashed = hash_password(user_data.password) # Encripta la contraseña
     user = User(
         name=user_data.name,
         username=user_data.username,
         email=user_data.email
     )
+    user.password = user_password_hashed # Le asigna la contraseña encriptada al usuario para guardarla en la bd
     db.add(user)
     db.commit()
     db.refresh(user)
