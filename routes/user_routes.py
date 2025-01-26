ea41a3ac8d2784d70d67fcea45c7240297f73dba
Edier55 <edierguerra55@gmail.com>
2025-01-25 20:41:06 -0500

    feat(routes): agrega nuevas rutas para el usuario
    - agrega ruta para generar un token
    
    - agrega una ruta protegida("api/v1/profile")

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,16 +1,20 @@
-from fastapi import Depends,APIRouter
+from fastapi import Depends,APIRouter,HTTPException
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
 from typing import Annotated
 from sqlalchemy.orm import Session
 from utils.hashing import hash_password
 from schemas import UserModel,CreateUserResponse
 from models import User
 from database import get_db
+from auth import  encode_token, get_user, validate_user
 
 route = APIRouter(prefix="/user")
 
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="user/token")
 
+# Registra un usuario
 @route.post("/create",response_model=UserModel)
-async def user(user_data:CreateUserResponse,db:Annotated[Session, Depends(get_db)]):
+async def create_user(user_data:CreateUserResponse,db:Annotated[Session, Depends(get_db)]):
     user_password_hashed = hash_password(user_data.password) # Encripta la contraseña
     user = User(
         name=user_data.name,
@@ -21,4 +25,21 @@ async def user(user_data:CreateUserResponse,db:Annotated[Session, Depends(get_db
     db.add(user)
     db.commit()
     db.refresh(user)
+    return user
+
+@route.post("/token")
+async def login(form_data:OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
+    user = validate_user(db,username=form_data.username,password=form_data.password)
+    if user == True:
+        token = encode_token(payload={"username":form_data.username})
+        return {
+            "access_token":token,
+            "token_type": "bearer"
+            }
+    else:
+        raise HTTPException(status_code=401,detail=user["message"])
+
+@route.get("/api/v1/profile" , response_model=UserModel)
+async def profile(db:Session = Depends(get_db),token:str = Depends(oauth2_schema)):
+    user = get_user(db,token)
     return user
