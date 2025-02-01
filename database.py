16e29faa98508258ca2bd9d448a0ff4312db55ae
Edier55 <edierguerra55@gmail.com>
2025-02-01 14:12:48 -0500

    Migra la base de datos a postgres

--- database.py
+++ database.py
@@ -4,17 +4,13 @@ from models import Base
 from decouple import config
 
 # url de base de datos
-URL_DB = f"mysql+pymysql://{config('MySQL_USER')}:{config('MySQL_PASSWORD')}@{config('MySQL_HOST')}:3306/{config('MySQL_DB')}"
+URL_DB = f"postgresql://{config('MySQL_USER')}:{config('MySQL_PASSWORD')}@{config('MySQL_HOST')}:3306/{config('MySQL_DB')}"
 
 # Motor de base de datos 
 engine = create_engine(url=URL_DB,echo=True)
 
 # Fabrica de sesiones
 SessionLocal = sessionmaker(bind = engine)
-
-if __name__ == "__main__":
-    # Iniciar la base de datos
-    Base.metadata.create_all(engine)
 
 # Dependencia para fastapi
 def get_db():
