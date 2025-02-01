b901921587e2f215901bc138f6c18fd30a16fc76
Edier55 <edierguerra55@gmail.com>
2025-02-01 16:18:18 -0500

    refactore(utils): mejora la logica del hashing de contraseñas

--- utils/hashing.py
+++ utils/hashing.py
@@ -3,10 +3,9 @@ from bcrypt import hashpw, checkpw, gensalt
 # Encripta la contraseña
 def hash_password(password:str):
     hashed_password = hashpw(password=password.encode("utf-8"), salt = gensalt())
-    return hashed_password
+    return hashed_password.decode("utf-8")
 
 # Valida la contraseña
 def validate_password(password:str,hashed_password:str):
-    valid_password = checkpw(password=password.encode("utf-8"),hashed_password=hashed_password.encode("utf-8"))
-    return valid_password
+    return checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
 
